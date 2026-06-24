a808aee8e18475a9baa420dc9049907b9b07113c
DESKTOP-QD5S814\jovan <23151318@aguascaliente.tecnm.mx>
2026-06-24 15:35:22 -0600

    Añadidas animaciones mágicas y mejoras visuales

--- js/script.js
+++ js/script.js
@@ -37,4 +37,20 @@ if (canvas) {
     resize();
     init();
     draw();
+    document.addEventListener("mousemove", function(e){
+
+    const sparkle = document.createElement("span");
+
+    sparkle.classList.add("sparkle");
+
+    sparkle.style.left = e.pageX + "px";
+    sparkle.style.top = e.pageY + "px";
+
+    document.body.appendChild(sparkle);
+
+    setTimeout(() => {
+        sparkle.remove();
+    }, 1000);
+
+});
 }
